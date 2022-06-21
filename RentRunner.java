6a60656ee558d15cff6011dd421fa8646b5493f7
Karunesh Ranjan <karunesh029@gmail.com>
2022-06-21 22:35:32 +0530

    Add files via upload

---
+++ RentRunner.java
@@ -0,0 +1,15 @@
+package com.masai.java;
+
+import java.util.ArrayList;
+
+public class RentRunner {
+	
+	static ArrayList<Customer> customerList = new ArrayList<Customer>();
+
+	public static void main(String[] args) {
+		Customer ramu = new Customer(1,"Ramu","Y","Z",1);
+		ramu.add();
+		ramu.print();
+	}
+
+}
